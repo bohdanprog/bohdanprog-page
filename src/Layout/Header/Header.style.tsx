914df3a74546becc63a6037eec2d05f6b459f31f
bohdan.prog <bohdan.artiukhov@blocklabs.eu>
2023-02-14 08:57:10 +0100

    add dist

--- src/Layout/Header/Header.style.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledHeader = styled.header`
-  width: 100%;
-  padding: 20px 0;
-  display: flex;
-`
--- src/Layout/Main/Main.style.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledMain = styled.main`
-  display: block;
-  flex-grow: 1;
-  width: 100%;
-  position: relative;
-`
